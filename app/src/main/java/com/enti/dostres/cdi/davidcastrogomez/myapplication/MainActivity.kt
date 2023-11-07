d39ed7bc5be9a96bf236ac22472c67e23c66a49f
David Castro Gómez <david.castro@enti.cat>
2023-11-07 09:53:20 +0100

    mainActivity setup

---
+++ app/src/main/java/com/enti/dostres/cdi/davidcastrogomez/myapplication/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.enti.dostres.cdi.davidcastrogomez.myapplication
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity(){
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity_screen)
+    }
+
+}
