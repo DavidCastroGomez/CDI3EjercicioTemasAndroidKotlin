e8f84a68e569d89c24b607af7a4dddc717a83115
David Castro Gómez <david.castro@enti.cat>
2023-11-20 11:09:28 +0100

    navegations

---
+++ app/src/main/java/com/enti/dostres/cdi/davidcastrogomez/myapplication/fragments/components/AppToolbar.kt
@@ -0,0 +1,39 @@
+package com.enti.dostres.cdi.davidcastrogomez.myapplication.fragments.components
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.enti.dostres.cdi.davidcastrogomez.myapplication.R
+import com.google.android.material.appbar.MaterialToolbar
+
+class AppToolbar : Fragment() {
+
+    companion object{
+        private lateinit var Instance: AppToolbar
+        fun get() = Instance
+    }
+
+    lateinit var toolbar: MaterialToolbar
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Instance = this
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.component_toolbar, container, false)
+        toolbar = view.findViewById(R.id.AppToolbar)
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
